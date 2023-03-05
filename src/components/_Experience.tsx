fe890bcf49c38296c939a3642cd59f152d240028
Zhixiang <zhixiang.peng@gmail.com>
2023-03-05 01:19:10 -0800

    Add GoogleMap Basic Function

--- src/components/_Experience.tsx
+++ src/components/_Experience.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  useJsApiLoader,
+} from '@react-google-maps/api';
+import { Box, Typography } from '@mui/material';
 
 const Experience = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey:
+      'AIzaSyBZ7bqUZaDTk7wp7UWh3JnbA2-55M21z5Q',
+  });
+  console.log(isLoaded);
+  if (!isLoaded) return <Typography>Loading</Typography>;
   return (
     <React.Fragment>
-      <div>Experience</div>
+      <Map></Map>
     </React.Fragment>
   );
 };
 
 export default Experience;
+
+const containerStyle = {
+  width: '500px',
+  height: '500px',
+};
+
+const center = { lat: 44, lng: -80 };
+
+const Map = () => {
+  return (
+    <Box
+      sx={{
+        mt: '5%',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={10}
+        center={center}
+        // onLoad={onLoad}
+        // onUnmount={onUnmount}
+      >
+        <Marker position={{ lat: 44, lng: -80 }} />
+      </GoogleMap>
+    </Box>
+  );
+};
